66a2ea6fd8b821701799ac8d4f63f336c01cfaac
Robert Hurst <robert@thinktankdesign.ca>
2011-12-04 22:17:07 -0800

    KeyboardJS can now be build with Ender.js. See http://ender.no.de/ for more info. fixes #6

--- demo.js
+++ demo.js
@@ -2,8 +2,37 @@ document.addEventListener('DOMContentLoaded', function (){
 
 	var exampleElement = document.getElementsByClassName('demoReadout')[0];
 
-	setInterval(function(){
+	//watch for konami code
+	var konami = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'],
+		kI = 0;
+
+
+	document.addEventListener('keydown', function(){
+
 		var keys = KeyboardJS.activeKeys();
+
+		if(keys.length) {
+			for(var i = 0; i < keys.length; i += 1) {
+
+				//check to see if the key is part of the konami code
+				if(keys[i] === konami[kI]) {
+					if(kI < konami.length - 1) {
+						kI += 1;
+					} else {
+						location = "http://en.wikipedia.org/wiki/Konami_Code";
+					}
+				} else {
+					kI = 0;
+				}
+
+			}
+		}
+	});
+
+	setInterval(function(){
+
+		var keys = KeyboardJS.activeKeys();
+
 		var keysString;
 		if(keys.length) {
 			keysString = keys.join(', ');
@@ -12,5 +41,5 @@ document.addEventListener('DOMContentLoaded', function (){
 		}
 		exampleElement.innerHTML = keysString;
 	}, 10);
-
+	
 });
