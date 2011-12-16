5e6cee14994417ce754ff17bc944fc1abc9fceb2
Robert Hurst <robert@thinktankdesign.ca>
2011-12-16 00:23:53 -0800

    Replaced the index.of polyfill with a ES5 compliant version. Cleaned up the addEvent function and renamed it bind.

--- keyboard.js
+++ keyboard.js
@@ -6,6 +6,12 @@
  * See license.txt
  */
 (function(context) {
+
+	//polyfills for ms's peice o' shit browsers
+	function bind(target, type, handler) { if (target.addEventListener) { target.addEventListener(type, handler, false); } else { target.attachEvent("on" + type, function(event) { return handler.call(target, event); }); } }
+	[].indexOf||(Array.prototype.indexOf=function(a,b,c){for(c=this.length,b=(c+~~b)%c;b<c&&(!(b in this)||this[b]!==a);b++);return b^c?b:-1;});
+
+	//keys
 	var keys = {
 			"backspace": 8,
 			"tab": 9,
@@ -56,56 +62,9 @@
 		activeKeys = [],
 		activeBindings = {},
 		keyBindingGroups = [];
-		
-	// IE prior to IE9 doesn't support addEventListener
-	var addEvent = function(target, type, handler) {
-		if (target.addEventListener)
-			target.addEventListener(type, handler, false);
-
-		else
-			target.attachEvent("on" + type, 
-								function(event) {
-									return handler.call(target, event);
-								});
-	};
-	
-	// oldIE (again, < IE9) can't handle .indexOf() on arrays, but Mozilla proposes the following
-	// solution: (https://developer.mozilla.org/en/JavaScript/Reference/Global_Objects/Array/indexOf#Compatibility)
-	if (!Array.prototype.indexOf) {
-	    Array.prototype.indexOf = function (searchElement /*, fromIndex */ ) {
-	        "use strict";
-	        if (this == null) {
-	            throw new TypeError();
-	        }
-	        var t = Object(this);
-	        var len = t.length >>> 0;
-	        if (len === 0) {
-	            return -1;
-	        }
-	        var n = 0;
-	        if (arguments.length > 0) {
-	            n = Number(arguments[1]);
-	            if (n != n) { // shortcut for verifying if it's NaN
-	                n = 0;
-	            } else if (n != 0 && n != Infinity && n != -Infinity) {
-	                n = (n > 0 || -1) * Math.floor(Math.abs(n));
-	            }
-	        }
-	        if (n >= len) {
-	            return -1;
-	        }
-	        var k = n >= 0 ? n : Math.max(len - Math.abs(n), 0);
-	        for (; k < len; k++) {
-	            if (k in t && t[k] === searchElement) {
-	                return k;
-	            }
-	        }
-	        return -1;
-	    }
-	}
 
 	//adds keys to the active keys array
-	addEvent(document, "keydown", function(event) {
+	bind(document, "keydown", function(event) {
 
 		//lookup the key pressed and save it to the active keys array
 		for (var key in keys) {
@@ -122,7 +81,7 @@
 	});
 
 	//removes keys from the active array
-	addEvent(document, "keyup", function (event) {
+	bind(document, "keyup", function (event) {
 
 		//lookup the key released and prune it from the active keys array
 		for(var key in keys) {
