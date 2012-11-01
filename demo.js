97937e4d25dda31f5923f30911f73c7a04cadaf8
Robert Hurst <robertwhurst@gmail.com>
2012-11-01 00:37:07 -0700

    Updated gh-pages with the KeyboardJS 0.3.0 docs

--- demo.js
+++ demo.js
@@ -1,45 +1,35 @@
-document.addEventListener('DOMContentLoaded', function (){
+$(function() {
+	var $demoReadout, konami, kI = 0;
 
-	var exampleElement = document.getElementsByClassName('demoReadout')[0];
+	$demoReadout = $('.demoReadout');
+	konami = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
 
-	//watch for konami code
-	var konami = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'],
-		kI = 0;
+	$(document).on('keydown', updateReadout);
+	$(document).on('keyup', updateReadout);
 
+	function updateReadout() {
+		setTimeout(function() {
+			var keys, keysString;
 
-	document.addEventListener('keydown', function(){
-
-		var keys = KeyboardJS.activeKeys();
-
-		if(keys.length) {
-			for(var i = 0; i < keys.length; i += 1) {
-
-				//check to see if the key is part of the konami code
-				if(keys[i] === konami[kI]) {
-					if(kI < konami.length - 1) {
-						kI += 1;
+			keys = KeyboardJS.activeKeys();
+			if(keys.length) {
+				keysString = keys.join(', ');
+				for(var i = 0; i < keys.length; i += 1) {
+					//check to see if the key is part of the konami code
+					if(keys[i] === konami[kI]) {
+						if(kI < konami.length - 1) {
+							kI += 1;
+						} else {
+							window.location = "http://en.wikipedia.org/wiki/Konami_Code";
+						}
 					} else {
-						location = "http://en.wikipedia.org/wiki/Konami_Code";
+						kI = 0;
 					}
-				} else {
-					kI = 0;
 				}
-
+			} else {
+				keysString = 'Press some keys...';
 			}
-		}
-	});
-
-	setInterval(function(){
-
-		var keys = KeyboardJS.activeKeys();
-
-		var keysString;
-		if(keys.length) {
-			keysString = keys.join(', ');
-		} else {
-			keysString = 'Press some keys...';
-		}
-		exampleElement.innerHTML = keysString;
-	}, 10);
-	
+			$demoReadout.html(keysString);
+		}, 0);
+	}
 });
