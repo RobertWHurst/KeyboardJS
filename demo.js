b5a81f70e32c8d9d812b7ee79a8cef5bc0406f02
Robert Hurst <RobertWHurst@Gmail.com>
2012-12-28 14:37:16 -0800

    Update demo.js

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ $(function() {
 	var $demoReadout, konami, kI = 0;
 
 	$demoReadout = $('.demoReadout');
-	$demoReadout.HTML('Press some keys...');
+	$demoReadout.html('Press some keys...');
 	konami = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
 
 	$(document).on('keydown', updateReadout);
