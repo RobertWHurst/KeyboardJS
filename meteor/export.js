879f34a240e40e9590273c5385cee5f2a6ed24b9
= <jp@cote.cc>
2015-02-12 11:29:56 -0500

    Removed meteor support.

--- meteor/export.js
+++
@@ -1,3 +0,0 @@
-/*global KeyboardJS:true*/  // Meteor.js creates a file-scope global for exporting. This comment prevents a potential JSHint warning.
-KeyboardJS = window.KeyboardJS;
-delete window.KeyboardJS;
